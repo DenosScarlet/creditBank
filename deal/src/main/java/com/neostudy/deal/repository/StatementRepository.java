737eedafd30323c58778b06a4ee2bc1e35189ab2
Давыдов Денис ИС-32 <dienis.davydov.2003@mail.ru>
2025-01-10 16:43:53 +0300

    deal/feature/Подключил МС к БД
    Подключил микросервис deal к БД PostreSQL и первый раз запустил приложение и создал таблицы через сущности

--- deal/src/main/java/com/neostudy/deal/repository/StatementRepository.java
+++ deal/src/main/java/com/neostudy/deal/repository/StatementRepository.java
@@ -1,6 +1,9 @@
 package com.neostudy.deal.repository;
 
+import com.neostudy.deal.entity.Statement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StatementRepository extends JpaRepository {
+import java.util.UUID;
+
+public interface StatementRepository extends JpaRepository<Statement, UUID> {
 }
